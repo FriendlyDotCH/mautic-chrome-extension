3d51413ffe408c1ca845d62e741a61cf8d3bd707
Brady Cargle <79949869+bradycargle@users.noreply.github.com>
2022-02-02 07:42:27 -0600

    Call functions when email compose is opened

--- background.js
+++ background.js
@@ -6,10 +6,11 @@ HOW TO CREATE MAUTIC/GMAIL CHROME EXTENSION
     * CSS to style this options page a little
     * Script page (background.js?) to contain the JS necessary
     * Manifest file
-    * Possibly an index.html that will pop up when someone click the extension icon. Could also put the options stuff in here
+    * Possibly an index.html that will pop up when someone click the extension icon
 
 2. Scripts page scripts (https://developer.chrome.com/docs/extensions/mv3/content_scripts/)
-    * Ideal outcome: Someone can send an email and the email will be added to Mautic in the user card history. Tracking opens and adding to card history would be cool too
+    * Ideal outcome: Someone can send an email and the email will be added to Mautic in the user card history. 
+        Tracking opens and adding to card history would be cool too
     * Create the content scripts (might need background scripts for some operations, not sure yet)
         - It sounds like I will need content scripts with something like this in manifest.json:
             o   "content_scripts": [ {
@@ -18,13 +19,16 @@ HOW TO CREATE MAUTIC/GMAIL CHROME EXTENSION
                     "css":          ["default.css"],
                     "js":           ["style-toggler.js"]
                 } ]
-        - Can be injected statically (in the manifest.json file, shown above) or programatically. For programatic insertion, extension needs host permissions for the page that it's injecting script into. For reference: https://developer.chrome.com/docs/extensions/mv3/content_scripts/
+        - Can be injected statically (in the manifest.json file, shown above) or programatically. 
+            For programatic insertion, extension needs host permissions for the page that it's injecting script into. 
+            For reference: https://developer.chrome.com/docs/extensions/mv3/content_scripts/
     * Algorithm for scripts:
         - Retrieve variables for Mautic URL and API connection
         - Get Mautic info and API connection from the user (get input)
         - Check is the user is on Gmail
         - Check if user is composing an email
-        - On email composition card, create a box that a user can check. This is to tell Mautic that an email has been sent to a contact
+        - On email composition card, create a box that a user can check. This is to tell Mautic that an email 
+            has been sent to a contact
         - Create a variable to store contact email
                 o connect this variable with the Mautic API and contacts
         - Use Mautic API to register a sent email to this contact
